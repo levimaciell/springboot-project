1d8e3f45f1f28ab60a18c06f7d8d257548c17639
levimaciell <levimaciell1221@gmail.com>
2023-06-11 09:58:52 -0300

    project set up

---
+++ src/test/java/com/levi/enterprises/spring/springProject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levi.enterprises.spring.springProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
