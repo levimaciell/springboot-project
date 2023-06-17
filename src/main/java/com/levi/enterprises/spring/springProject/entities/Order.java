74f64a64e8f94e3e3f736abd29fa25cae713fea6
levimaciell <levimaciell1221@gmail.com>
2023-06-17 15:09:50 -0300

    Enum OrderStatus

--- src/main/java/com/levi/enterprises/spring/springProject/entities/Order.java
+++ src/main/java/com/levi/enterprises/spring/springProject/entities/Order.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.levi.enterprises.spring.springProject.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -24,7 +24,7 @@ public class Order implements Serializable{
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
-    //private OrderStatus orderStatus
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -33,10 +33,11 @@ public class Order implements Serializable{
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);;
     }
 
     public Long getId() {
@@ -53,6 +54,16 @@ public class Order implements Serializable{
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode(); 
+        }
     }
 
     public User getClient() {
@@ -87,11 +98,5 @@ public class Order implements Serializable{
             return false;
         return true;
     }
-
-    
-
-
-
-
 
 }
