7966d03a7dc8e53183467cf90bfc1150255e8823
levimaciell <levimaciell1221@gmail.com>
2023-06-20 16:45:41 -0300

    payment, one-to-one association

--- src/main/java/com/levi/enterprises/spring/springProject/config/testConfig.java
+++ src/main/java/com/levi/enterprises/spring/springProject/config/testConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.levi.enterprises.spring.springProject.entities.Category;
 import com.levi.enterprises.spring.springProject.entities.Order;
 import com.levi.enterprises.spring.springProject.entities.OrderItem;
+import com.levi.enterprises.spring.springProject.entities.Payment;
 import com.levi.enterprises.spring.springProject.entities.Product;
 import com.levi.enterprises.spring.springProject.entities.User;
 import com.levi.enterprises.spring.springProject.entities.enums.OrderStatus;
@@ -79,6 +80,12 @@ public class testConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());	
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
+
     }
 
 }
